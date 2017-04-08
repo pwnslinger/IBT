0dddcfc76ac5ac3ab7d017ce104d50ed5248002b
mohsen ahmadi <ahmadi@agpp.ir>
2017-04-09 01:51:31 +0430

    manually resolved conflicts of Moh branch with master.

--- ibt.py
+++ ibt.py
@@ -65,21 +65,34 @@ class IdaBackTracer:
                 
                 else:
                     if value in op1:
-                        if value in op1:
-                            if idaapi.o_reg is idaapi.cmd.Op2.type and 'eax' in GetOpnd(address,1):
-                                hasCall, c, adr = self.hasCallInst(address,10)
-                                if hasCall:
-                                    print '%s found as a candidate for DS initialization %d instructions after %s' % (GetFunctionName(GetOperandValue(address,0)), c, idc.GetDisasm(address))
-                                    if self.checkInit(GetOperandValue(adr,0)):
-                                        print '%s contains pointer to a heap allocated memory region %s' % (GetOpnd(address,1) , GetDisasm(address))
-
+                        if idaapi.o_reg is idaapi.cmd.Op2.type and 'eax' in GetOpnd(address,1):
+                            hasCall, c, adr = self.hasCallInst(address,0)
+                            if hasCall:
+                                print '%s found as a candidate for DS initialization %d instructions after %s' % (GetFunctionName(GetOperandValue(address,0)), c, idc.GetDisasm(address))
+                                if checkInit(GetOperandValue(adr,0)):
+                                    print '%s contains pointer to a heap allocated memory region %s' % (GetOpnd(address,1) , GetDisasm(address))
                         print '%s: %s %s -> %s' % (hex(address),mn,op1,op2)
                         return self.trace_reg(address,op2)
                         
             address=PrevHead(address,minea=0)
             
-        return None                 
+    def hasCallInst(self, address, counter):
+        return
+			
+    '''
+    this function actually checks whether a specific                             function routine contains any invocation to heap         allocation routines such like 'GetProcessHeap','HeapAlloc'. If there's then return True    
+    '''
     
+    @staticmethod
+    def checkInit(adr):
+        call_list, heap_flag = traverseCalls(adr)
+        if heap_flag:
+            return True
+        for funcName in call_list:
+            funcAddr = LocByName(funcName)
+            return checkInit(funcAddr)
+        return False
+
     @staticmethod
     def get_arg(address, argument_number):
         # It traces back maximum 10 instructions
@@ -87,7 +100,7 @@ class IdaBackTracer:
         argument_counter = 0
         if GetMnem(address) != 'call':
             return None
-        
+
         for inst_count in range(0, 11):
             address = PrevHead(address, minea=0)
             
@@ -96,111 +109,8 @@ class IdaBackTracer:
                 
             if argument_counter == argument_number:
                     return address
-
-    @staticmethod
-    def hasCallInst(address,count):
-        for c in range(0, count + 10):
-            address = PrevHead(address,minea=0)
-            mn = GetMnem(address)
-            if 'call' in mn:
-                return True, c, address
             
-        return False, None, None
-        
-
-# this function actually checks whether a specific function routine contains any invocation to heap allocation routines such like 'GetProcessHeap','HeapAlloc'. If there's then return True
-    def checkInit(self, adr):
-        
-        call_list, heap_flag = traverseCalls(adr)
-        if heap_flag:
-            return True
-        for funcName in call_list:
-            funcAddr = LocByName(funcName)
-            return checkInit(funcAddr)
-        return False
-        
-'''
-My desired result would be like this one:
-
-{'sub_100019B0',[{'sub_1000331F',[[{'GetProcessHeap':None},'10006058'],[{'HeapAlloc':None},'10006054']]},'1000331F']}
-'''	
-
-#do a double-check plz
-'''
-def traverseCalls(adr):
-    start=GetFunctionAttr(adr,FUNCATTR_START)
-    end=GetFunctionAttr(adr,FUNCATTR_END)
-    address=NextHead(start,maxea=end)
-    key=GetFunctionName(address)
-    while start <= address <= end:
-        mn = GetMnem(address)
-        if 'call' in mn:
-            name=GetFunctionName(GetOperandValue(address,0))
-            if name is None:
-                name=GetOpnd(address,0)
-                    
-            adrr=GetOperandValue(address,0)
-            if !FirstR:
-            if callee.has_key(key) == False:
-                if ('GetProcessHeap','HeapAlloc') in name:
-                    return {name[3:]:None}
-                else:
-                    FirstR = True
-                    callee[key]=[traverseCalls(adrr),hex(adrr)]
-                
-            elif ('GetProcessHeap','HeapAlloc') in name:
-                return {name[3:]:None}
-                else:
-                    callee[key].append(traverseCalls(adrr),hex(adrr))
-        address = NextHead(address,maxea=end)
-        else:
-            return None
-    return callee
-'''
-
-def traverseCalls(adr):
-    ''' 
-    print 'entering into %s' % GetFunctionName(adr)
-    print 'searching for heap_alloc calls inside'
-    
-    flags=GetFunctionFlags(adr)
-    start=GetFunctionAttr(adr,FUNCATTR_START)
-    address = PrevHead(adr,minea=0)
-    while start>= adr: 
-        
-    end=GetFunctionAttr(adr,FUNCATTR_END)
-    call_list=[]
-    heap_found=False
-    
-    #ignore library functions
-    if flags & idaapi.FUNC_THUNK or flags  & idaapi.FUNC_LIB:
-        return 
-    
-    #get list all ea's of current function routine
-    disasm_addr = list(idautils.FuncItems(adr))
-    
-    for ea in disasm_addr:
-    
-        if idaapi.is_call_insn(ea):
-            op_addr = GetOperandValue(ea,0)
-            op_type = GetOpType(ea,0)
-            name=GetFunctionName(op_addr)
-            op_flags = GetFunctionFlags(op_addr)            
-            
-            if op_flags & idaapi.FUNC_LIB:
-                name = Name(op_addr)
-                if name in ('GetProcessHeap','HeapAlloc'):
-                    print 'Heap allocation routine found at %s' % GetFunctionName(ea)
-                    heap_found=True
-                    call_list.append(name)
-                    break
-                    
-            call_list.append(name)
-    '''
-    call_list=[]
-    heap_found = False
-    return call_list, heap_found
-
+        return None
             
 def main():
     ibt = IdaBackTracer()
